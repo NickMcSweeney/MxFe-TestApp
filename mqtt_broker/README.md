eaaeac213910a93cfa5014c3adadaad293e55f3d
Nicholas Shindler <shindler25@gmail.com>
2023-02-27 09:50:27 +0100

    add mqtt broker

---
+++ mqtt_broker/README.md
@@ -0,0 +1,28 @@
+# `keyser`
+> `hudson` message broker
+
+# How to build
+
+    yarn install
+
+    yarn build
+
+## How to use
+
+Before doing anything, make sure you run `yarn install` to install all dependencies.
+
+### Configuration
+
+Make sure to set the following environment variables (eg. in `.env`) to configure the application:
+
+| Environment Variable | Default Value |
+|-------|-------|
+| MESSAGE_LOGGER_ENABLED | `false` |
+
+### `yarn dev`
+
+Runs the app in the development mode.
+
+### `yarn start`
+
+Runs the app in pro mode
---
+++ mqtt_broker/src/index.ts
@@ -0,0 +1,62 @@
+require('dotenv').config()
+console.log("MESSAGE_LOGGER_ENABLED=" + (process.env.MESSAGE_LOGGER_ENABLED ?? false))
+
+import {AedesPublishPacket, Client, Subscription} from "aedes";
+import * as protobuf from "protobufjs";
+//var { protobuf } = require('protobufjs');
+
+console.log((new Date()).toISOString() + ":", "Starting keyser ...")
+console.log((new Date()).toISOString() + ":", "Initializing protobuf ...")
+protobuf.load("datastructs.proto", (error, root) => {
+  if (error) {
+    console.error((new Date()).toISOString() + ":", "Error loading protobuf:", error)
+    throw error;
+  }
+  if (root === undefined) {
+    console.error((new Date()).toISOString() + ":", "protobuf root undefined! There may be an issue with the protobuf schema itself.")
+    throw new Error("protobuf root undefined! There may be an issue with the protobuf schema itself.");
+  }
+
+  const aedes = require('aedes')()
+  const { createServer } = require('aedes-server-factory')
+  const wsPort = 1883
+  const httpPort = 1884
+
+  const wsServer = createServer(aedes, { ws: true })
+  const httpServer = createServer(aedes)
+
+  wsServer.listen(wsPort, function () {
+    console.log((new Date()).toISOString() + ":", 'MQTT WS server listening on port ' + wsPort)
+  })
+  httpServer.listen(httpPort, function () {
+    console.log((new Date()).toISOString() + ":", 'MQTT HTTP started and listening on port ' + httpPort)
+  })
+
+  // fired on subscribe
+  aedes.on('subscribe', (subscriptions: Subscription[], client: Client) => {
+    console.log((new Date()).toISOString() + ":", 'MQTT client \x1b[32m' + (client ? client.id : client) +
+      '\x1b[0m subscribed to topic(s): ' + subscriptions.map(s => s.topic).join('\n'), 'from broker', aedes.id)
+  })
+
+  // fired on unsubscribe
+  aedes.on('unsubscribe', (subscriptions: string[], client: Client) => {
+    console.log((new Date()).toISOString() + ":", 'MQTT client \x1b[32m' + (client ? client.id : client) +
+      '\x1b[0m unsubscribed from topics: ' + subscriptions.join('\n'), 'from broker', aedes.id)
+  })
+
+  // fired when a client connects
+  aedes.on('client', (client: Client) => {
+    console.log((new Date()).toISOString() + ":", 'Client Connected: \x1b[33m' + (client ? client.id : client) + '\x1b[0m', 'to broker', aedes.id)
+  })
+
+  // fired when a client disconnects
+  aedes.on('clientDisconnect', (client: Client) => {
+    console.log((new Date()).toISOString() + ":", 'Client Disconnected: \x1b[31m' + (client ? client.id : client) + '\x1b[0m', 'to broker', aedes.id)
+  })
+
+  // fired when a message is published
+  if (process.env.MESSAGE_LOGGER_ENABLED ?? false)
+    aedes.on('publish', async (packet: AedesPublishPacket, client: Client) => {
+      console.log((new Date()).toISOString() + ":", 'Client \x1b[31m' + (client ? client.id : 'BROKER_' + aedes.id) + '\x1b[0m has published a packet of size', packet.payload.length, 'on', packet.topic, 'to broker', aedes.id)
+    })
+});
